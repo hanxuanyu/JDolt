06694dd478bd2930fe7a178781acd9dc04ced932
hxuanyu <2252193204@qq.com>
2024-12-23 09:29:58 +0800

    init commit

---
+++ jdolt-core/src/main/java/com/hxuanyu/doltmanager/Test.java
@@ -0,0 +1,13 @@
+package com.hxuanyu.doltmanager;
+
+/**
+ * TODO
+ *
+ * @author hanxuanyu
+ * @version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
---
+++ jdolt-service/src/main/java/com/hxuanyu/doltmanager/DoltManagerApplication.java
@@ -0,0 +1,17 @@
+package com.hxuanyu.doltmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TODO
+ *
+ * @author hanxuanyu
+ * @version 1.0
+ */
+@SpringBootApplication
+public class DoltManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DoltManagerApplication.class, args);
+    }
+}
